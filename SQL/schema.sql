c453fd90078c7128d281ccb7df59378fa1e820c8
Daniel Perez Garnica <danielperezgarnic@gmail.com>
2022-09-07 08:44:27 -0500

    starting today right

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -2,28 +2,28 @@ drop database if exists employees_db;
 create database employees_db;
 use employees_db;
 
+create table departments(
+    departmentID int not null,
+    department varchar(50) not null,
+    primary key (departmentID)
+);
+
+create table roles(
+    roleID int not null,
+    jobTitle varchar(50) not null,
+ADDTHEREFERENCETO THE departmentID TOKNOWWHEREITSCOMINGFROM
+    departmentID int not null, 
+    salary int not null
+    primary key (roleID)
+);
+
+
 create table employeeProfile(
     id int not null,
     firstName varchar(50) not null,
     lastName varchar(50) not null,
     jobTitle varchar(50) not null,
-    salary int not null,
     manager varchar(50) ,
-    department varchar(50) not null,
-    employeeRole varchar(50) not null
-
-);
-
-create table departments(
-    departmentID int not null,
-    department varchar(50) not null
-);
-
-create table roles(
     roleID int not null,
-    employeeRole varchar(50) not null,
-    jobTitle varchar(50) not null,
-    department varchar(50) not null,
-    salary int not null
+    primary key (id)
 );
-
