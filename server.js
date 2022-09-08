c0cd9e1a07044f69128f7e1dd6f4d184d39c1e0d
Daniel Perez Garnica <danielperezgarnic@gmail.com>
2022-09-08 13:21:28 -0500

    p r o g r e s s

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+require('console.table');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const db = mysql.createConnection(
   {
@@ -18,12 +14,35 @@ const db = mysql.createConnection(
 );
 
 // Add your db.queries aqui pls
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'initialPrompt',
+      message: 'What do you wanna do??',
+      choices: ['View all employees', 'View all roles', 'View all departments'],
+    },
+  ]) .then(answers => {
+    console.log(answers);
+    if (answers.initialPrompt == 'View all employees') {
+      console.log('employees');
+    }
+    if (answers.initialPrompt == 'View all roles') {
+      console.log('roles');
+    }
+    if (answers.initialPrompt == 'View all departments') {
+      seeDepartments();
+    }
+  })
 
-app.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
+  // create ur finctions aqui pls
+
+function seeDepartments() {
+  db.query('SELECT * FROM departments', (err, results) => {
+    if (err) {
+      console.log('error');
+    } 
+    console.table(results);
+    console.log(results);
+  })
+}
